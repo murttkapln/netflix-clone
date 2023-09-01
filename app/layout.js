32af031fd7d271d72cf78cc6512562a6568f79f3
Murat <murttkapln@gmail.com>
2023-09-01 17:23:05 +0300

    navbar

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 // import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +15,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Navbar />
-        {children} */}
+        <Navbar />
+        {children}
       </body>
     </html>
   );
