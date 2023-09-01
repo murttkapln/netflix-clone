05b72a24a3c09c1e20cb307286e514f947c6f2a2
Murat <murttkapln@gmail.com>
2023-09-01 14:58:14 +0300

    Home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,28 @@
+import Link from "next/link";
+//? app folder içindeki page.js "/" route olan home sayfasıdır
+
+
+//? in next.js all components are default server components. If we want to use client components we have to use "use-client" direct.
+
+
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Next.js</h1>
+    <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+    <div className="bg-black w-full h-full bg-opacity-60">
+      <div className="text-white text-center relative top-2/4 m-auto">
+        <h1 className="text-5xl font-[900] text-center mb-3">
+          Unlimited movies, TV shows, and more
+        </h1>
+        <p className="text-2axl font-[400] ">
+          Watch anywhere. Cancel anytime.
+        </p>
+        {/* //? next/link arka planda sayfayı önceden fetch edilir. Bu, client tarafı gezintilerin performansını iyileştirmek için kullanışlıdır. Görünüm alanındaki herhangi bir <Link />  önceden yüklenecektir. */}
+        <Link href="/register">
+          <button className="btn-danger w-[250px]">Get Started</button>
+        </Link>
+      </div>
     </div>
+  </div>
   );
 }
